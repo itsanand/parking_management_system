531d30aa5821c68ee792ffab2e1c41d5bfccdba2
itsanand <cu.18bcs2174@gmail.com>
2023-10-01 00:08:36 +0530

    feat(parking_system): initiated django server

---
+++ parking_management_system/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for parking_management_system project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+from __future__ import annotations
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'parking_management_system.settings',
+)
+
+application = get_wsgi_application()
